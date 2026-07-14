6fc6f065587c49e2f6372e3421f96cd0327d382f
Nikolai <ne360cosmos@gmail.com>
2026-07-15 00:03:16 +0300

    Создан проект

---
+++ service/src/test/java/com/example/reactiveratingservice/ReactiveRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRatingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
